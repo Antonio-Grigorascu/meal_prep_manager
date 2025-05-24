388744a5eab2d8a559e35a837fb430b60cdaf6d9
AntonioGrigorascu <grigorascu.antonio@gmail.com>
2025-05-24 21:24:03 +0300

    DB Connection and DB Init

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -22,6 +22,7 @@ public class DBConnection {
             e.printStackTrace();
         }
     }
+    
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
